09372419216daf957f072d93a6f8227f903d96ae
BERNAT Loan <loantravail@gmail.com>
2020-12-20 17:23:31 +0100

    SDL fonctionnelle !

--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
-#include <SDL2/SDL.h>
+#include <SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
- 
-int main(int argc, char *argv[])
+
+/* 
+    Commande de compilation avec gcc :
+    gcc src/main.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2
+*/ 
+
+int main(int argc, char **argv)
 {
+    SDL_version nb;
+    SDL_VERSION(&nb);
+
+    printf("Bienvenue sur la DSL %d.%d.%d !\n", nb.major, nb.minor, nb.patch);
+
+    return 0;
 }
  
